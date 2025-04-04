0fd95cfcc91ea37cbe08f8c8edebcf7b0e63beca
Ritu Bafna <ritujane78@gmail.com>
2025-04-04 18:01:19 +0545

    make orderhistory service

---
+++ src/app/sevices/order-history.service.ts
@@ -0,0 +1,24 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { OrderHistory } from '../common/order-history';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class OrderHistoryService {
+  private orderUrl = 'http://localhost:9090/api/orders/';
+
+  constructor(private httpClient: HttpClient) {}
+  getOrderHistory(theEmail: string): Observable<GetResponseOrderHistory>{
+    let orderSearchUrl = `${this.orderUrl}search/findByCustomerEmail?email=${theEmail}`;
+
+    return this.httpClient.get<GetResponseOrderHistory>(orderSearchUrl);
+
+  }
+}
+interface GetResponseOrderHistory{
+  _embedded: {
+    orders: OrderHistory[];
+  }
+}
